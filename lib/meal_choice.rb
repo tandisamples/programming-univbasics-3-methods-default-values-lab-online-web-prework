06cf785224aa28c1ccf96a24541181b1736f293e
Tandi Samples <tandisamples@gmail.com>
2019-08-07 23:02:18 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,8 @@
-# Your code here
+def 
+  puts "What a nutritious meal!"
+ => "What a nutritious meal!"
+ 
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
+ => "A plate of #{protein} with #{veg1} and #{veg2}."
+ 
+ end
