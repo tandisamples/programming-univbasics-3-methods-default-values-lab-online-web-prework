8ab39c2194492491c96fc53b7202371911723c91
Tandi Samples <tandisamples@gmail.com>
2019-08-07 23:02:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@ def
   puts "What a nutritious meal!"
  => "What a nutritious meal!"
  
-puts "A plate of #{protein} with #{veg1} and #{veg2}."
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
  => "A plate of #{protein} with #{veg1} and #{veg2}."
  
  end
